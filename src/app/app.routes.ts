452f249e4da19c46b05e26e38df11d23c89572fc
Shivansh Srivastava <srishivansh2019@gmail.com>
2025-06-19 16:06:15 +0530

    final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ProductsComponent },
+];
